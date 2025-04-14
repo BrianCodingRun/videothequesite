ed7b179dc646cf269f34e8369bc73ca7053c4175
BrianCodingRun <dev.contact@briancoupama.re>
2025-04-14 11:48:59 +0400

    Version dynamique avec intéraction api terminé

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,49 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
 import Navbar from "@/components/navbar";
 import Grid from "@/components/grid";
 import Card from "@/components/card";
 
+type Movie = {
+  id: string;
+  title: string;
+  image: string;
+  description: string;
+};
+
 export default function Home() {
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await fetch("https://ghibliapi.vercel.app/films");
+        const data = await response.json();
+        console.log(data);
+        if (data.length > 0) {
+          setMovies(data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchMovies();
+  }, []);
+
   return (
     <>
       <Navbar />
       <main className="max-w-[1200px] mx-auto flex flex-col items-center sm:items-start">
         <Grid>
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {movies.map((movie) => (
+            <Card
+              key={movie.id}
+              poster={movie.image}
+              title={movie.title}
+              description={movie.description}
+            />
+          ))}
         </Grid>
       </main>
     </>
--- components/card.tsx
+++ components/card.tsx
@@ -7,23 +7,26 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 
-export default function card() {
+export default function card({
+  poster,
+  title,
+  description,
+}: {
+  poster: string;
+  title: string;
+  description: string;
+}) {
   return (
     <Card className="relative w-[300px] bg-transparent border-none">
       <CardHeader>
-        <img
-          src="https://images.justwatch.com/poster/307575554/s718/avengers-4.jpg"
-          alt="Card image cap"
-        />
+        <img src={poster} alt="Card image cap" />
       </CardHeader>
       <CardContent className="flex flex-col">
-        <CardTitle className="text-xl font-semibold">
-          Avengers: Endgame
-        </CardTitle>
+        <CardTitle className="text-xl font-semibold">{title}</CardTitle>
         <CardDescription>
-          Avengers: Endgame is an American superhero film series developed by
-          Marvel Studios. The films are the sequel to Avengers: Infinity War
-          (2018) and Avengers: Age of Ultron (2015)...
+          {description.length > 100
+            ? description.slice(0, 100) + "..."
+            : description}
         </CardDescription>
       </CardContent>
     </Card>
